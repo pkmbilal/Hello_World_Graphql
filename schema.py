c989104dde9431696b9ca55a0a84df89f2f5d814
Mohammed Bilal <bilal00717@gmail.com>
2024-05-04 11:58:41 +0300

    GraphQL mutations added.

--- schema.py
+++ schema.py
@@ -26,4 +26,12 @@ class Query:
     users: typing.List[User] = strawberry.field(resolver=get_users)
 
 
-schema = strawberry.Schema(query=Query)
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def add_user(self, id: int, name: str, email: str, phone: str) -> str:
+        user_data.append({'id': id, 'name': name, 'email': email, 'phone': phone})
+        return f"User {name} added successfully."
+
+
+schema = strawberry.Schema(query=Query, mutation=Mutation)
