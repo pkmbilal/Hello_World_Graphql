0f864f57d8fb4a147f8b7ae78600bb02f933ddd0
Mohammed Bilal <bilal00717@gmail.com>
2024-05-18 09:08:38 +0300

    Fast api default code added for testing.

--- schema.py
+++ schema.py
@@ -1,37 +1,19 @@
 import strawberry
-import typing
 
-
-user_data = [
-    {"id": 1, "name": "Mohammed Bilal", "email": "bilal00717@gmail.com", "phone": "0543831060"},
-    {"id": 2, "name": "Ameen", "email": "ameen@gmail.com", "phone": "0544837257"},
-]
-
-
-@strawberry.type
-class User:
-    id: int
-    name: str
-    email: str
-    phone: str
-
-
-def get_users():
-    users = [User(**data) for data in user_data]
-    return users
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
 
 @strawberry.type
 class Query:
-    users: typing.List[User] = strawberry.field(resolver=get_users)
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
 
 
-@strawberry.type
-class Mutation:
-    @strawberry.mutation
-    def add_user(self, id: int, name: str, email: str, phone: str) -> str:
-        user_data.append({'id': id, 'name': name, 'email': email, 'phone': phone})
-        return f"User {name} added successfully."
+schema = strawberry.Schema(Query)
 
+graphql_app = GraphQLRouter(schema)
 
-schema = strawberry.Schema(query=Query, mutation=Mutation)
+app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")
